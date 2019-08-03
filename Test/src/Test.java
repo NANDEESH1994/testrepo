8b8af0c0e5df4ff1b53023f47fedf9361d2f9b1c
Nandeesh <Nandeesh@Nandeesh-PC>
2019-08-04 01:27:27 +0530

    testcommit

---
+++ Test/src/Test.java
@@ -0,0 +1,21 @@
+public class Test
+{
+
+	public static void main  (String args[])
+	{
+ 	
+	
+	
+ for (int a=1 ; a<7 ; ++a)
+ {
+	 
+	 for (int b=1 ; b<a ; ++b)
+	 {
+		 System.out.print(b + " ");
+		 
+	 }
+	 System.out.println();
+ }
+	 
+	}
+}
